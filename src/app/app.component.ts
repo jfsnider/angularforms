47f5f7825f23ee59984aa70fd56d46b5faf8766c
jfnsnider <jfsnider@winsupplyinc.com>
2023-03-30 15:07:19 -0400

    registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularforms';
+  defaultState = 'ohio';
+
+  firstname!: string;
+  lastname!: string;
+  streetAddress!: string;
+  emailAddress!: string;
+  state!: string;
+
+  @ViewChild('myForm') form!: NgForm;
+
+
+
+
+  onSubmit() {
+    console.log(this.form);
+
+    this.firstname = this.form.value.personDetails.firstname;
+    this.lastname = this.form.value.personDetails.lastname;
+    this.streetAddress = this.form.value.personDetails.address;
+    this.state = this.form.value.personDetails.stateselect;
+    this.emailAddress = this.form.value.personDetails.email;
+
+    this.form.reset();
+  }
+
 }
